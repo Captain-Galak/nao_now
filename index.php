a6da3c679f03fb14f0e3a7f3416c746547a32446
Captain-Galak <admin@hellomiso.fr>
2018-10-03 16:13:59 +0200

    init commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>NAO NOW ILLUSTRATIONS</title>
+
+	<link rel="stylesheet" href="css/bootstrap.css">	
+	
+	<!-- CSS Vendor -->
+	<link rel="stylesheet" type="text/css" href="css/normalize.css" media="all" />
+	<!-- CSS Perso -->
+	<link rel="stylesheet" type="text/css" href="css/style.css?timestamp=<?php echo time() ?> " media="screen" />
+	<script src="http://code.jquery.com/jquery-3.3.1.js" 
+	integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+	crossorigin="anonymous">
+	</script>
+
+	<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+</head>
+<body>
+	
+<div class="container-fluid">
+	<div class="construction_wrapper">
+		<h1 class="display-4 text-center">En Construction</h1>
+	</div>
+
+	<div class="construction_pic text-center">
+	<img  class="w-75 h-75" src="visuels/enconstruction.jpg" alt="Patience !">
+	</div>
+
+	<div class="RS_wrapper tewt text-center">
+		<p class="lead"> Retrouvez moi sur vos réseaux favoris:</p>
+		<div class="nav justify-content-center">
+			
+				<li class="nav-item"><a href="https://www.instagram.com/nao_now_illustrations/"><img src="visuels/insta.png" alt=""></a></li>
+
+				<li class="nav-item"><a href="https://www.facebook.com/NaoRei"><img src="visuels/facebook.png" alt=""></a></li>
+				<li class="nav-item"><a href="https://twitter.com/Nao_nau_now"><img src="visuels/twitter.png" alt=""></a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
